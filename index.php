85b65705555e89e2271654a21d4ad9f618a3e1d0
Nguyễn Thái Dương <123867834+duogxaolin@users.noreply.github.com>
2023-06-02 18:10:13 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Translate Page</title>
+
+    <!-- Thêm thẻ <script> của Google Translate API -->
+<link href="duogxaolin.css?t=1685691435175" rel="stylesheet" type="text/css" />
+<script type="text/javascript">
+    function googleTranslate() {
+        new google.translate['TranslateElement']({
+            pageLanguage: 'vi'
+        }, 'button_translate');
+    }
+</script>
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslate"></script>
+</head>
+<body>
+    <h1>Welcome to my website!</h1>
+
+    <!-- Thêm phần tử cho Google Translate -->
+   <div id="button_translate"></div>
+
+    <h2>Content</h2>
+    <p>This is the content of the page.</p>
+    <p>...</p>
+</body>
+</html>
